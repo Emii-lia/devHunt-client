aae1f13bff03657a3ce89cba65ccd84aa55b8f05
Emii-lia <fiaromiangaly@gmail.com>
2024-03-07 04:20:45 +0300

    feat: add nav list for landing navbar

---
+++ src/utils/nav.routes.ts
@@ -0,0 +1,21 @@
+import { INavRoute } from "../types/nav.types";
+
+export const navRoutes:INavRoute[] = [
+    {
+        path:"/",
+        label: "Home",
+    },
+    {
+        path:"/info",
+        label: "Information",
+    },
+    {
+        path:"/news",
+        label: "Blog",
+    },
+
+    {
+        path:"/aide",
+        label: "Aide",
+    },
+]
