67893f34d720879855a69573ab6f1d6656b39d3a
Emii-lia <fiaromiangaly@gmail.com>
2024-03-07 04:10:15 +0300

    feat: add button components

---
+++ src/components/button/post.button.tsx
@@ -0,0 +1,22 @@
+import { IUser } from "../../types/user.types"
+import image from '../../assets/image/100790.png'
+import Avatar from "../userProfile/components/avatar/avatar"
+import { Button } from "./button"
+import { useNavigate } from "react-router-dom"
+
+type PostButtonProps = {
+    style?: string
+}
+export const PostButton = ({style}:PostButtonProps) => {
+     // const {userInfo} = useContext(UserContext)
+     const navigate = useNavigate()
+     const userInfo:Partial<IUser> = {
+        profileUrl:image
+    }
+    return (
+        <div className="flex flex-row space-x-10 items-center justify-center w-full">
+            <Avatar url={userInfo.profileUrl!}/>
+            <Button label="Partager votre experience..." onClick={()=>navigate("post/create")} type="button" variant="neutral" style={style}/>
+        </div>
+    )
+}
